a9919f6beba5e42b3e95e64a70df99ed47bbeeef
Abhishek Jha <iamabhishek1310@gmail.com>
2026-08-26 14:16:16 +0530

    Added comp and sales tables

---
+++ app/models/company.py
@@ -0,0 +1,58 @@
+import uuid
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, relationship
+from app.models.sales import Base
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Company(Base):
+    __tablename__ = "Company"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
+    
+    company_name = Column(String)
+    company_email = Column(String)
+    company_phone = Column(String)
+    company_address = Column(String)
+    company_city = Column(String)
+    company_state = Column(String)
+    company_zip = Column(String)
+    company_country = Column(String)
+    company_logo = Column(String)
+    company_website = Column(String)
+    company_employees = Column(Integer)
+    
+
+    employees = relationship("Employee", back_populates="company")
+    sales = relationship("Sales", back_populates="company")
+
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    
+
+class Employee(Base):
+    __tablename__ = "Employee"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
+    
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+    phone = Column(String)
+    address = Column(String)
+    zip = Column(String)
+    country = Column(String)
+    role = Column(String)
+    compensation = Column(Integer)
+    
+    company_id = Column(UUID(as_uuid=True), ForeignKey("Company.id"))    
+    
+    company = relationship("Company", back_populates="employees")
+    
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
---
+++ app/models/sales.py
@@ -0,0 +1,29 @@
+import uuid
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from app.models.company import Base
+
+class Sales(Base):
+    __tablename__ = "Sales"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
+
+    product = Column(String)
+    quantity = Column(Integer)
+    price = Column(Integer)
+    import_date = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    total_sales = Column(Integer)
+    total_revenue = Column(Integer)
+    total_profit = Column(Integer)
+    
+    total_loss = Column(Integer)
+    
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+    company_id = Column(UUID(as_uuid=True), ForeignKey("Company.id"))
+
+    company = relationship("Company", backref="sales")
